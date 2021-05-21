33ab394d48b5690edc191740db01bfd3053c9308
Jason Lam <nyjasonlam@gmail.com>
2021-05-21 12:05:22 -0400

    Added trips, locations, travelers routes to routes/index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const travelerRoutes = require("./traveler-routes");
-// const tripRoutes = require('./trip-routes');
-// const locationRoutes = require('./location-routes');
+const tripRoutes = require('./trip-routes');
+const locationRoutes = require('./location-routes');
 
 router.use("/travelers", travelerRoutes);
+router.use("/locations", locationRoutes);
+router.use("/trips", tripsRoutes);
 
 module.exports = router;
