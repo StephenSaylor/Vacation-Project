69a7e1788db75c476db63d1ad058331341188887
Jason Lam <nyjasonlam@gmail.com>
2021-05-21 12:12:17 -0400

    Added some comments

--- routes/api/trip-routes.js
+++ routes/api/trip-routes.js
@@ -3,26 +3,31 @@ const { Trip, Traveler } = require("../../models");
 
 // GET all travelers and all Trips
 router.get("/", async (req, res) => {
+  // TODO: route logic here!
   res.status(500).send("500 Internal Server Error.");
 });
 
 // GET travelers by id and their Trips
 router.get("/:id", async (req, res) => {
+  // TODO: route logic here!
   res.status(500).send("500 Internal Server Error.");
 });
 
 // POST create new traveler
 router.post("/", async (req, res) => {
+  // TODO: route logic here!
   res.status(500).send("500 Internal Server Error.");
 });
 
 // PUT update traveler
 router.put("/:id", async (req, res) => {
+  // TODO: route logic here!
   res.status(500).send("500 Internal Server Error.");
 });
 
 // DELETE traveler
 router.delete("/:id", async (req, res) => {
+  // TODO: route logic here!
   res.status(500).send("500 Internal Server Error.");
 });
 
