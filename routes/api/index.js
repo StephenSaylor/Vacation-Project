2312e9383832e3752dc25885e59061ce7ced7b48
Jason Lam <nyjasonlam@gmail.com>
2021-05-21 12:15:37 -0400

    Fixed router in api/index

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,6 @@ const locationRoutes = require('./location-routes');
 
 router.use("/travelers", travelerRoutes);
 router.use("/locations", locationRoutes);
-router.use("/trips", tripsRoutes);
+router.use("/trips", tripRoutes);
 
 module.exports = router;
